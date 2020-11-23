fab883518255b9d584fddedbfa05740db54c5aca
bmheenan <bmheenan@users.noreply.github.com>
2020-11-22 19:58:22 -0500

    - Updated connection string for prod

--- config_example.go
+++ config_example.go
@@ -5,10 +5,10 @@ package tapdb
 // This file is commented off. A copy of this file, `config.go` is marked in .gitignore to protect the actual db
 // credentials. Recreate it locally by uncommenting this file and filling in the details below
 
-func getTestCredentials() (user string, pass string, instance string) {
+func getTestCredentials() (user, pass, connName string) {
 	user = "**********"
 	pass = "**********"
-	instance = "**********"
+	connName = "**********"
 	return
 }
 */
--- init.go
+++ init.go
@@ -15,7 +15,7 @@ var _ mysql.Config // keep the linter from cleaning up the import. sql.Open need
 // Init initialized the database connection, creates the database and tables if needed, then returns an interface
 // with all functions that can be executed against the database
 // The username and password of the database must be provided
-func Init(user, pass string) (DBInterface, error) {
+func Init(user, pass, connName string) (DBInterface, error) {
 	var (
 		unixSocket,
 		host,
@@ -24,7 +24,7 @@ func Init(user, pass string) (DBInterface, error) {
 	)
 	if os.Getenv("GAE_INSTANCE") != "" {
 		// Running in prod
-		unixSocket = "/cloudsql/"
+		unixSocket = "/cloudsql"
 	} else {
 		// Running locally. Config for the Cloud SQL proxy
 		// https://cloud.google.com/sql/docs/mysql/quickstart-proxy-test
@@ -33,7 +33,7 @@ func Init(user, pass string) (DBInterface, error) {
 	}
 	db := &mysqlDB{}
 	// First connection has no db specified so it doesn't have to exist; we'll create it with this connection
-	crDBconn, errOp0 := sql.Open("mysql", fmtOpenStr(user, pass, host, port, unixSocket, ""))
+	crDBconn, errOp0 := sql.Open("mysql", fmtOpenStr(user, pass, host, port, unixSocket, connName, ""))
 	if errOp0 != nil {
 		return &mysqlDB{}, fmt.Errorf("Could not establish mysql db connection: %v", errOp0)
 	}
@@ -48,7 +48,7 @@ func Init(user, pass string) (DBInterface, error) {
 	// Second persistent connection specifies the DB now that we just created it. Subsequent queries will already be
 	// using the db this way
 	var errOp1 error
-	db.conn, errOp1 = sql.Open("mysql", fmtOpenStr(user, pass, host, port, unixSocket, dbName))
+	db.conn, errOp1 = sql.Open("mysql", fmtOpenStr(user, pass, host, port, unixSocket, connName, dbName))
 	if errOp1 != nil {
 		return &mysqlDB{}, fmt.Errorf("Could not establish mysql db connection: %v", errOp1)
 	}
@@ -63,7 +63,7 @@ func Init(user, pass string) (DBInterface, error) {
 }
 
 // Format the connection string for both prod and local dev
-func fmtOpenStr(user, pass, host, port, unixSocket, dbName string) string {
+func fmtOpenStr(user, pass, host, port, unixSocket, connName, dbName string) string {
 	var cred string
 	if user != "" {
 		cred = user
@@ -73,7 +73,7 @@ func fmtOpenStr(user, pass, host, port, unixSocket, dbName string) string {
 		cred = cred + "@"
 	}
 	if unixSocket != "" {
-		return fmt.Sprintf("%sunix(%s)/%s", cred, unixSocket, dbName)
+		return fmt.Sprintf("%s@unix(/%s/%s)/%s?parseTime=true", cred, unixSocket, connName, dbName)
 	}
 	return fmt.Sprintf("%stcp([%s]:%s)/%s", cred, host, port, dbName)
 }
